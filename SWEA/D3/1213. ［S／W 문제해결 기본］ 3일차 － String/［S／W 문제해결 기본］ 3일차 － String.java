a1949a8821f0c34c987b5125f2b2f30e682ed393
diarlee <97464327+diarlee@users.noreply.github.com>
2023-08-14 17:53:34 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 152 ms, Memory: 23,236 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
-
-class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		for(int test_case = 1; test_case <= 10; test_case++) {
-			int n = sc.nextInt();
-			int cnt1 = 0;
-			int cnt2 = 0;
-			String str1 = sc.next(); // 비교할 문자열 입력 받기
-			String snt = sc.next(); // 문장 입력 받기
-			for (int i = 0; i < snt.length() - str1.length() + 1; i++) {
-				cnt1 = 0;
-				// 문장속에 첫 번째 문자가 비교 문자열의 첫 번째 문자열과 동일한 경우에만 계산 수행
-				if (str1.charAt(0) == snt.charAt(i)) {
-					cnt1++; // 각 문자 비교 횟수
-					for (int j = i; j < str1.length() + i - 1; j++) {
-						if (str1.charAt(1 + j - i) == snt.charAt(j + 1))
-							cnt1++; // 각 문자 비교 횟수
-					}
-				}
-				if (cnt1 == str1.length()) // 모든 문자가 같다면
-					cnt2++;
-			}
-			System.out.println("#" + test_case + " " + cnt2);
-		}
-	}
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = 10;
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            sc.nextInt();
+            String[] key = sc.next().split("");
+            int K = key.length;
+            String[] sentence = sc.next().split("");
+            int S = sentence.length;
+            int cnt = 0;
+            int i = 0;
+            while (i < S) {
+                if (sentence[i].equals(key[0]) && i <= S - K) {
+                    int check = 0;
+                    for (int j = 1; j < K; j++) {
+                        if (!(key[j].equals(sentence[i + j]))) {
+                            check = 1;
+                            break;
+                        }
+                    }
+                    if (check == 0) {
+                        i += K;
+                        cnt++;
+                    }
+                }
+                i++;
+            }
+            System.out.println("#" + test_case + " " + cnt);
+        }
+    }
 }
