49752a086cf3949f59e602bc7560aee8480ff213
diarlee <97464327+diarlee@users.noreply.github.com>
2023-08-14 23:59:00 +0900

    [D2] Title: 초심자의 회문 검사, Time: 123 ms, Memory: 19,988 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
+import java.io.FileInputStream;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int test_case = 1; test_case <= T; test_case++) {
-			String str = sc.next(); // 문자열 입력 받기
-			int ans = 1;
-			for (int i = 0; i < str.length() / 2; i++) {
-				if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
-					ans = 0;
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			String[] arr = sc.next().split("");
+			int N = arr.length;
+			int check = 1;
+			for (int i = 0; i < arr.length / 2; i++) { 
+				if (!(arr[i].equals(arr[N - 1 - i]))) { 
+					check = 0;
 					break;
 				}
 			}
-			System.out.println("#" + test_case + " " + ans);
+			if (check == 0) System.out.println("#" + test_case + " " + check);
+			else System.out.println("#" + test_case + " " + check);
 		}
 	}
 }
