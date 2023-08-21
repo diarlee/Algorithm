4d4623be82dc2d4667e91ca5b32b48aa1de674db
diarlee <97464327+diarlee@users.noreply.github.com>
2023-08-22 00:29:40 +0900

    [Silver V] Title: 수들의 합 5, Time: 236 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-    	// 내 풀이
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int st = 1, ed = 1;
-        int cnt = 1; // 자기자신 포함
-        int sum = st;
-        while (st <= N / 2) {
-        	// st값을 증가시켜가며 st ~ ed값의 합이 N인지 확인
-        	ed++;
-        	sum += ed;
-        	if (sum == N) cnt++;
-        	if (sum > N) {
-        		st++;
-        		ed = st;
-        		sum = st;
-        	}
-        }
-        System.out.println(cnt);
-    }
+	public static void main(String[] args) {
+		// 교재 풀이 // 투 포인터
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int st = 1, ed = 1;
+		int sum = 1;
+		int cnt = 1;
+		while (ed != N) {
+			if (sum == N) {
+				cnt++;
+				ed++;
+				sum += ed;
+			}
+			if (sum < N) { // 오른쪽 포인터 이동
+				ed++;
+				sum += ed;
+			}
+			if (sum > N) { // 왼쪽 포인터 이동
+				sum -= st;
+				st++;
+			}
+		}
+		System.out.println(cnt);
+	}
 }
