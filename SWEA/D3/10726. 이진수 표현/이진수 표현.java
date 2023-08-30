d382735dd2d0173369070ebbca748916ef0c0820
diarlee <97464327+diarlee@users.noreply.github.com>
2023-08-30 12:17:21 +0900

    [D3] Title: 이진수 표현, Time: 304 ms, Memory: 50,152 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -1,28 +1,19 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
+	// M & 하위 N비트만 1인 수 == 하위 N비트만 1인 수 : M의 하위 N비트가 다 1이다!
+	// 하위 N비트만 1인 수  : 1 << N - 1
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for (int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt(); 
 			int M = sc.nextInt();
-			int[] bits = new int[30];
-			int idx = 29;
-			while (M > 0) {
-				bits[idx--] = M % 2;
-				M /= 2;
-			}
 			String ans = "ON";
-			for (int i = 29; i > 29 - N; i--) {
-				if (bits[i] != 1) {
-					ans = "OFF";
-					break;
-				}
+			if ((M & ((1 << N) - 1)) != ((1 << N) - 1)) { // 이진수 비교 // 출력하면 십진수로
+				ans = "OFF";
 			}
 			System.out.println("#" + tc + " " + ans);
 		}
 	}
 }
-// Integer.toBinaryString(1) -> 1
