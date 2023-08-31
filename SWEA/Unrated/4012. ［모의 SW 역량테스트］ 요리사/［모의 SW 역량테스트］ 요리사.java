671600bc5eb9049a220628742916d8c17b5a957f
diarlee <97464327+diarlee@users.noreply.github.com>
2023-08-31 14:40:35 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 234 ms, Memory: 40,472 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,66 +1,55 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
-class Solution {
+public class Solution {
+	public static int[] list = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 	public static int N;
-	public static int[][] bd;
-	public static int[] sl;
-	public static int[] sx;
-	public static int totsum;
-	public static int mini;
-
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+	public static int R;
+	public static int[][] infos;
+	public static int[] sel;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
 		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
-			bd = new int[N][N];
-			sl = new int[N / 2];
-			sx = new int[N / 2];
-			mini = Integer.MAX_VALUE;
-			totsum = 0;
+			N = sc.nextInt();
+			R = N / 2;
+			infos = new int[N][N];
 			for (int i = 0; i < N; i++) {
-				String[] tempLine = br.readLine().split(" ");
 				for (int j = 0; j < N; j++) {
-					int temp = Integer.parseInt(tempLine[j]);
-					bd[i][j] = temp;
-					totsum += temp;
+					infos[i][j] = sc.nextInt();
 				}
 			}
-			fc(0, 0);
-			System.out.println("#" + tc + " " + mini);
+			sel = new int[R];
+			List<Integer> sumResult = new ArrayList<>();
+			comb(0, 0, sumResult);
+			int min = Integer.MAX_VALUE;
+			for (int i = 0; i < sumResult.size() / 2; i++) { // i번째 부분집합과 2^^R - 1번째 부분집합을 합치면 전체 집합
+				int differ = Math.abs(sumResult.get(i) - sumResult.get(sumResult.size() - 1 - i));
+				if (differ < min) min = differ;
+			}
+			System.out.println("#" + tc + " " + min);
 		}
 	}
-
-	public static void fc(int idx, int sidx) {
-		if (sidx == N / 2) {
-			int s1 = 0;
-			int s2 = 0;
-			for (int i = 0; i < N / 2; i++) {
-				for (int j = 0; j < i; j++) {
-					s1 += bd[sl[i]][sl[j]]+bd[sl[j]][sl[i]] ;
+	
+	
+	public static void comb(int n, int r, List<Integer> sumResult) { // n개 중 r개 뽑아서 리스트에 담기
+		if (r == R) { // 부분집합 완성
+			int sel1Sum = 0; // 부분집합 시너지합 구하기
+			for (int i = 0; i < R - 1; i++) {
+				for (int j = i + 1; j < R; j++) {
+					sel1Sum += infos[sel[i]][sel[j]];
+					sel1Sum += infos[sel[j]][sel[i]];
 				}
 			}
-			for (int i = 0, k = 0; i < N; i++) {
-				if (k < N / 2 && sl[k] == i) {
-					k++;
-					continue;
-				}
-				for (int j = 0, l = 0; j<i ; j++) {
-					if (l < N / 2 && sl[l] == j) {
-						l++;
-						continue;
-					}
-					s2 += bd[i][j]+bd[j][i];
-				}
-			}
-			mini = Math.min(Math.abs(s2 - s1), mini);
+			sumResult.add(sel1Sum);
 			return;
 		}
-		for (int i = idx; i <= (N / 2) + sidx; i++) {
-			sl[sidx] = i;
-			fc(i + 1, sidx + 1);
-		}
+		if (n == N) return;
+		sel[r] = list[n]; // 현재 원소 n
+		comb(n + 1, r + 1, sumResult); // n 포함
+		comb(n + 1, r, sumResult); // n 미포함
 	}
 }
