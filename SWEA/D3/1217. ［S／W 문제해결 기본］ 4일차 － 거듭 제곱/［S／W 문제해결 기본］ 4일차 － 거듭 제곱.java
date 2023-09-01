c7a935b9b0c090c3b6a08e9c65271f659bad75ea
diarlee <97464327+diarlee@users.noreply.github.com>
2023-09-02 01:46:53 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 130 ms, Memory: 20,396 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
+
+public class Solution {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = 10;
-		for(int test_case = 1; test_case <= T; test_case++)
-		{	
-            sc.nextInt();
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-			System.out.println("#" + test_case + " " + doubleMulti(N, M));
+		for (int tc = 1; tc <= 10; tc++) {
+			sc.nextInt();
+			int N = sc.nextInt(), M = sc.nextInt();
+			int[] stored = new int[M + 1];
+			System.out.println("#" + tc + " " + pow(N, M, stored));
 		}
 	}
-	public static int doubleMulti(int n, int m) {
-		if (m == 1) return n;
-		return n * doubleMulti(n, m - 1);
+	// 한 번 계산한 값은 저장하면서 재귀
+	public static int pow(int N, int M, int[] list) {
+		if (list[M] != 0) return list[M]; 
+		if (M == 1) {
+			list[M] = N;
+			return list[M];
+		}
+		if (M % 2 == 0) { // 짝수일때
+			if (list[M] == 0) list[M] = pow(N, M / 2, list) * pow(N, M / 2, list);
+			return list[M];
+		}
+		else { // 홀수일때
+			if (list[M] == 0) list[M] = pow(N, M / 2, list) * pow(N, M / 2, list) * N;
+			return list[M];
+		}
 	}
 }
