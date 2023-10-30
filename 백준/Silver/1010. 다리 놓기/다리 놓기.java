5f76dbef4df5adce8911da1c0fb5512bc49f355e
diarlee <97464327+diarlee@users.noreply.github.com>
2023-10-31 00:33:28 +0900

    [Silver V] Title: 다리 놓기, Time: 168 ms, Memory: 15704 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int[][] dp = new int[M + 1][N + 1];
+			for (int i = 1; i <= M; i++) dp[i][0] = 1;
+			for (int i = 1; i <= M; i++) {
+				for (int j = 1; j <= N; j++) {
+					if (i == j) dp[i][j] = 1;
+					if (i > j) dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+				}
+			}
+			System.out.println(dp[M][N]);
+		}
+	}
+}
