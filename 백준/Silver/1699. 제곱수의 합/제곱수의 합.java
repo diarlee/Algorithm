1d84a34517f2e67b4dd130ca068dddbaf65fc991
diarlee <97464327+diarlee@users.noreply.github.com>
2024-01-06 01:15:20 +0900

    [Silver II] Title: 제곱수의 합, Time: 216 ms, Memory: 14876 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[100001];
+
+        for (int i = 1; i <= Math.sqrt(100000); i++) {
+        	dp[i * i] = 1;
+        }
+        
+        for (int i = 1; i <= 100000; i++) {
+        	dp[i] = i;
+        	int num = 2;
+        	while (Math.pow(num, 2) <= i) {
+        		dp[i] = Math.min(dp[i], dp[i - (int) Math.pow(num, 2)] + 1); 
+        		num++;
+        	}
+        }
+        
+        System.out.println(dp[N]);
+    }
+}
