8c54ce70db325abed5e5cd7de0262c7c993cf807
diarlee <97464327+diarlee@users.noreply.github.com>
2024-01-21 12:53:26 +0900

    [Silver IV] Title: 반복수열, Time: 140 ms, Memory: 19876 KB -BaekjoonHub

---
+++ 백준/Silver/2331. 반복수열/반복수열.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int P;
+	static int[] D;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		D = new int[1000000];
+		visited = new boolean[1000000];
+		
+		int curNum = A;
+		// 반복되는 수가 나올때까지 수열 진행
+		while (D[curNum] == 0) {
+			int nextNum = getNextNum(curNum);
+			D[curNum] = nextNum;
+			curNum = nextNum;
+		}
+		
+		// 수열 중 반복수열 부분 구분하기
+		while (!visited[curNum]) {
+			visited[curNum] = true;
+			curNum = D[curNum];
+		}
+		
+		// 수열 중 반복수열 부분 제외한 나머지 부분 구하기
+		int cnt = 0;
+		for (int i = 1; i < 1000000; i++) {
+			if (D[i] != 0 && !visited[i]) cnt++;
+		}
+		System.out.println(cnt);
+		
+	}
+	
+	// 수열의 다음 수 구하기
+	static int getNextNum(int curNum) {
+		List<Integer> list = new ArrayList<>();
+		while (curNum > 0) {
+			list.add(curNum % 10);
+			curNum /= 10;
+		}
+		int nextNum = 0;
+		for (int n : list) {
+			nextNum += (int) Math.pow(n, P);
+		}
+		return nextNum;
+	}
+}
