e28cd9ee0a1df3eb3328e8215383e4ee76521b70
diarlee <97464327+diarlee@users.noreply.github.com>
2024-03-12 21:31:53 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
 SELECT d.dept_id, d.dept_name_en, round(avg(e.SAL)) as avg_sal
-FROM hr_department d RIGHT OUTER JOIN hr_employees e
+FROM hr_department d INNER JOIN hr_employees e
 ON d.dept_id = e.dept_id
 GROUP BY d.dept_id
 ORDER BY avg_sal DESC;
