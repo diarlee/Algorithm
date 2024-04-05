2ac0ae11f60de990bad758e6aede1b5c339dd8c6
diarlee <97464327+diarlee@users.noreply.github.com>
2024-04-05 16:16:25 +0900

    [Silver II] Title: 외판원 순회 2, Time: 372 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[][] costs;
+	static boolean[] visited;
+	static int min;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		costs = new int[N][N];
+		visited = new boolean[N];
+		min = Integer.MAX_VALUE;
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				costs[i][j] = Integer.parseInt(st.nextToken()); 
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			visited[i] = true;
+			dfs(i, i, 0, 0);
+			visited[i] = false;
+		}
+		System.out.println(min);
+	}
+	
+	static void dfs(int st, int city, int level, int sum) {
+		if (level == N - 1 && costs[city][st] != 0) {
+			min = Math.min(min, sum + costs[city][st]);
+			return;
+		}
+		if (level == N - 1) {
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if (i != st && costs[city][i] != 0 && !visited[i]) {
+				visited[i] = true;
+				dfs(st, i, level + 1, sum + costs[city][i]);
+				visited[i] = false;
+			}
+		}
+	}
+}
