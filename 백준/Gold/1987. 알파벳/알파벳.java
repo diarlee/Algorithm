9fd4730086cba435e4265e0f05eba5eb8c412729
diarlee <97464327+diarlee@users.noreply.github.com>
2024-09-04 21:31:19 +0900

    [Gold IV] Title: 알파벳, Time: 1160 ms, Memory: 16620 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int R;
+	static int C;
+	static char[][] map;
+	static boolean[][] visited;
+	static boolean[] checked;
+	static int[] dr = {0, 1, 0, -1};
+	static int[] dc = {1, 0, -1, 0};
+	static int cnt;
+	static int result;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+		for (int i = 0; i < R; i++) {
+			String letters = br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = letters.charAt(j);
+			}
+		}
+		visited = new boolean[R][C];
+		checked = new boolean[26];
+		cnt = 0;
+		result = 0;
+		dfs(0, 0);
+		System.out.println(result);
+	}
+	
+	static void dfs(int r, int c) {
+		visited[r][c] = true;
+		checked[map[r][c] - 'A'] = true;
+		cnt++;
+		result = Math.max(result, cnt);
+		int nr, nc;
+		for (int i = 0; i < 4; i++) {
+			nr = r + dr[i];
+			nc = c + dc[i];
+			if (0 <= nr && nr < R && 0 <= nc && nc < C && !visited[nr][nc] 
+					&& !checked[map[nr][nc] - 'A']) {
+				dfs(nr, nc);
+			}
+		}
+		cnt--;
+		checked[map[r][c] - 'A'] = false;
+		visited[r][c] = false;
+	}
+}
